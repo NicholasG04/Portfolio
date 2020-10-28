fcf1afdaef5ae2d6f2292f75c39ea9972cd6f596
Nick <33908215+NicholasG04@users.noreply.github.com>
2020-10-28 19:38:24 +0000

    Added blog page in lieu of directory listing (not for general use yet...)

---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,43 @@
+import { NextSeo } from 'next-seo';
+import { getAllPosts } from '../../lib/graphcms';
+import BlogCard from '../../components/BlogCard';
+import NavBar from '../../components/NavBar';
+
+export async function getStaticProps() {
+  const posts = await getAllPosts();
+  return {
+    props: { posts },
+    revalidate: 60,
+  };
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <NextSeo
+        title="Nicholas G - Blog"
+        openGraph={{
+          title: 'Nicholas G - Blog',
+        }}
+      />
+      <NavBar dark />
+      <h1>Nicholas G - Blog //</h1>
+      <div className="posts">
+        {posts.map((post) => (
+          <BlogCard key={post.slug} slug={post.slug} title={post.title} excerpt={post.excerpt} coverImage={post.coverImage} date={post.date} />
+        ))}
+      </div>
+      <style jsx>{`
+        h1 {
+          text-align: center;
+          font-size: 3rem;
+        }
+        .posts {
+          display: flex;
+          flex-flow: row wrap;
+        }
+      `}
+      </style>
+    </>
+  );
+}
