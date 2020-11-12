80a1ae8f92b992e922ec0c6776bf78c58493286f
Nick <33908215+NicholasG04@users.noreply.github.com>
2020-11-12 16:38:27 +0000

    Changed to GraphQL document variables

--- src/lib/graphcms.tsx
+++ src/lib/graphcms.tsx
@@ -10,15 +10,15 @@ interface Post {
   }
 }
 
-export async function getPostAndMoreBySlug(slug: string): Promise<{ posts: Post[]; morePosts: Post[] }> {
+export async function getPostAndMoreBySlug(slug: string): Promise<{ post: Post; morePosts: Post[] }> {
   const graphcms = new GraphQLClient(process.env.GRAPHCMS_PROJECT_API, {
     headers: {
       authorization: `Bearer ${process.env.GRAPHCMS_PROD_AUTH_TOKEN}`,
     },
   });
   const { post, morePosts }: {post: Post; morePosts: Post[]} = await graphcms.request(`
-    query getPostAndMoreBySlug {
-      post(where: {slug: "${slug}"}) {
+    query getPostAndMoreBySlug($slug: String!) {
+      post(where: {slug: $slug}) {
         title
         slug
         date
@@ -38,7 +38,7 @@ export async function getPostAndMoreBySlug(slug: string): Promise<{ posts: Post[
           })
         }
       }
-      morePosts: posts(orderBy: date_DESC, first: 2, where: {slug_not: "${slug}"}) {
+      morePosts: posts(orderBy: date_DESC, first: 2, where: {slug_not: $slug}) {
         title
         slug
         date
@@ -56,7 +56,7 @@ export async function getPostAndMoreBySlug(slug: string): Promise<{ posts: Post[
         }
       }
     }
-  `);
+  `, { slug });
   return { post, morePosts };
 }
 
@@ -105,4 +105,54 @@ export async function getAllSlugs(): Promise<Post[]> {
     }
   `);
   return posts;
+}
+
+export async function getPreviewPostBySlug(slug: string) {
+  const graphcms = new GraphQLClient(process.env.GRAPHCMS_PROJECT_API, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_DEV_AUTH_TOKEN}`,
+    },
+  });
+  const { post, morePosts }: {post: Post; morePosts: Post[]} = await graphcms.request(`
+    query getPreviewPostBySlug($slug: String!) {
+      post(where: {slug: $slug}) {
+        title
+        slug
+        date
+        content {
+          markdown
+        }
+        excerpt
+        coverImage {
+          url(transformation: {
+            image: {
+              resize: {
+                fit: crop,
+                width: 1000,
+                height: 500
+              }
+            }
+          })
+        }
+      }
+      morePosts: posts(orderBy: date_DESC, first: 2, where: {slug_not: $slug}) {
+        title
+        slug
+        date
+        excerpt
+        coverImage {
+          url(transformation: {
+            image: {
+              resize: {
+                fit: crop,
+                width: 300,
+                height: 150
+              }
+            }
+          })
+        }
+      }
+    }
+  `, {slug});
+  return { post, morePosts };
 }
